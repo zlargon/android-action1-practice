0b8a5835747e1915cd08224e33bfd991ae9f315f
leon_huang <leon_huang@gemteks.com>
2014-08-03 10:36:32 +0800

    [10:36] p.183-191

---
+++ src/com/zlargon/action1/ContainerFragment.java
@@ -0,0 +1,15 @@
+package com.zlargon.action1;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ContainerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.container, container, false);
+    }
+}
